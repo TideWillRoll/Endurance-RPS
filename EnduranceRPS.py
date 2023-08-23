8daef8ca47bb0fecfe4f03d5423cb682f3aa82f9
TideWillRoll <wtc19@comcast.net>
2023-08-23 14:27:21 -0400

    EnduranceRPS

--- EnduranceRPS.py
+++ EnduranceRPS.py
@@ -1,14 +1,14 @@
 import random
+player_score = 0
+computer_score = 0
 def play():
+    global player_score, computer_score
     choices = ('Rock', 'Paper', 'Scissors')
     user_choice = input(choices)
     computer_choice = random.choice(choices)
     is_win = (user_choice == 'Rock' and computer_choice == 'Scissors') or (user_choice == 'Paper' and computer_choice == 'Rock') or (user_choice == 'Scissors' and computer_choice == 'Paper')
     is_loss = (user_choice == 'Rock' and computer_choice == 'Paper') or (user_choice == 'Paper' and computer_choice == 'Scissors') or (user_choice == 'Scissors' and computer_choice == 'Rock')
-    tie = (user_choice == 'Rock' and computer_choice == 'Rock') or (user_choice == 'Paper' and computer_choice == 'Paper') or (user_choice == 'Scissors' and computer_choice == 'Scissors') 
-    print(f"You chose {user_choice}, Computer chose {computer_choice}")
-    player_score = 0
-    computer_score = 0
+    tie = user_choice == computer_choice
     def score():
         print("You", player_score, "Computer", computer_score)
     if is_win:
@@ -17,9 +17,9 @@ def play():
         score()
         play()
     elif is_loss:
-        computer_score += 1
+        if player_score > 0:
+             player_score = 0 and computer_score == 0
         print("You lost! Better luck next time!")
-        score()
         script()
     elif tie:
         player_score += 0; computer_score += 0
